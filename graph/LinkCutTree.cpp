147f0d57f69aa98b65dfa991c9b154ae9dee5e3d
TaWeiTu <tu.da.wei@gmail.com>
2018-06-03 13:00:00 +0800

    LinkCutTree + FFT

---
+++ graph/LinkCutTree.cpp
@@ -0,0 +1,109 @@
+struct node {
+    node *ch[2], *fa, *pfa;
+    int sum, v, rev;
+    node(int s): v(s), sum(s), rev(0), fa(nullptr), pfa(nullptr) {
+        ch[0] = nullptr;
+        ch[1] = nullptr;
+    }
+    int relation() {
+        return this == fa->ch[0] ? 0 : 1;
+    }
+    void push() {
+        if (!rev) return;
+        swap(ch[0], ch[1]);
+        if (ch[0]) ch[0]->rev ^= 1;
+        if (ch[1]) ch[1]->rev ^= 1;
+        rev = 0;
+    }
+    void pull() {
+        sum = v;
+        if (ch[0]) sum += ch[0]->sum;
+        if (ch[1]) sum += ch[1]->sum;
+    }
+    void rotate() {
+        if (fa->fa) fa->fa->push();
+        fa->push(), push();
+        swap(pfa, fa->pfa);
+        int d = relation();
+        node *t = fa;
+        if (t->fa) t->fa->ch[t->relation()] = this;
+        fa = t->fa;
+        t->ch[d] = ch[d ^ 1];
+        if (ch[d ^ 1]) ch[d ^ 1]->fa = t;
+        ch[d ^ 1] = t;
+        t->fa = this;
+        t->pull(), pull();
+    }
+    void splay() {
+        while (fa) {
+            if (!fa->fa) {
+                rotate();
+                continue;
+            }
+            fa->fa->push(), fa->push();
+            if (relation() == fa->relation()) fa->rotate(), rotate();
+            else rotate(), rotate();
+        }
+    }
+    void evert() {
+        access();
+        splay();
+        rev ^= 1;
+    }
+    void expose() {
+        splay(), push();
+        if (ch[1]) {
+            ch[1]->fa = nullptr;
+            ch[1]->pfa = this;
+            ch[1] = nullptr;
+            pull();
+        }
+    }
+    bool splice() {
+        splay();
+        if (!pfa) return false;
+        pfa->expose();
+        pfa->ch[1] = this;
+        fa = pfa;
+        pfa = nullptr;
+        fa->pull();
+        return true;
+    }
+    void access() {
+        expose();
+        while (splice());
+    }
+    int query() {
+        return sum;
+    } 
+};
+
+namespace lct {
+    node *sp[maxn];
+    void make(int u, int v) {
+        // create node with id u and value v
+        sp[u] = new node(v, u);
+    }
+    void link(int u, int v) {
+        // u become v's parent
+        sp[v]->evert();
+        sp[v]->pfa = sp[u];
+    }
+    void cut(int u, int v) {
+        // u was v's parent
+        sp[u]->evert();
+        sp[v]->access(), sp[v]->splay(), sp[v]->push();
+        sp[v]->ch[0]->fa = nullptr;
+        sp[v]->ch[0] = nullptr;
+        sp[v]->pull();
+    }
+    void modify(int u, int v) {
+        sp[u]->splay();
+        sp[u]->v = v;
+        sp[u]->pull();
+    }
+    int query(int u, int v) {
+        sp[u]->evert(), sp[v]->access(), sp[v]->splay();
+        return sp[v]->query();
+    }
+} 
--- math/FFT.cpp
+++ math/FFT.cpp
@@ -1,31 +1,54 @@
+const int maxn = 131072;
+using cplx = complex<double>;
+const cplx I = cplx(0, 1);
 const double pi = acos(-1);
-const complex<double> I(0, 1);
-complex<double> omega[maxn + 1];
+cplx omega[maxn + 1];
 
 void prefft() {
     for (int i = 0; i <= maxn; ++i) omega[i] = exp(i * 2 * pi / maxn * I);
 }
 
-void fft(vector<complex<double>>& a, int n, bool inv = false) {
-    int basic = maxn / n;
-    int theta = basic;
-    for (int m = n; m >= 2; m >>= 1) {
-        int h = m >> 1;
-        for (int i = 0; i < h; ++i) {
-            complex<double> w = omega[inv ? maxn - (i * theta % maxn) : i * theta % maxn];
-            for (int j = i; j < n; j += m) {
-                int k = j + h;
-                complex<double> x = a[j] - a[k];
-                a[j] += a[k];
-                a[k] = w * x;
+void bin(vector<cplx> &a, int n) {
+    int lg;
+    for (lg = 0; (1 << lg) < n; ++lg); --lg;
+    vector<cplx> tmp(n);
+    for (int i = 0; i < n; ++i) {
+        int to = 0;
+        for (int j = 0; (1 << j) < n; ++j) to |= (((i >> j) & 1) << (lg - j));
+        tmp[to] = a[i];
+    }
+    for (int i = 0; i < n; ++i) a[i] = tmp[i];
+}
+
+void fft(vector<cplx> &a, int n) {
+    bin(a, n);
+    for (int step = 2; step <= n; step <<= 1) {
+        int to = step >> 1;
+        for (int i = 0; i < n; i += step) {
+            for (int k = 0; k < to; ++k) {
+                cplx x = a[i + to + k] * omega[maxn / step * k];
+                a[i + to + k] = a[i + k] - x;
+                a[i + k] += x;
             }
         }
-        theta = (theta * 2) % maxn;
     }
-    int i = 0;
-    for (int j = 1; j < n - 1; ++j) {
-        for (int k = n >> 1; k > (i ^= k); k >>= 1);
-        if (j < i) swap(a[i], a[j]);
-    }
-    if (inv) for (int i = 0; i < n; ++i) a[i] /= (double)n;
+}
+
+void ifft(vector<cplx> &a, int n) {
+    fft(a, n);
+    reverse(a.begin() + 1, a.end());
+    for (int i = 0; i < n; ++i) a[i] /= n;
+}
+
+vector<int> multiply(vector<int> &a, vector<int> &b, int n) {
+    vector<cplx> ca(n << 1), cb(n << 1);
+    for (int i = 0; i < n; ++i) ca[i] = cplx(a[i], 0);
+    for (int i = 0; i < n; ++i) cb[i] = cplx(b[i], 0);
+    fft(ca, n << 1); fft(cb, n << 1);
+    vector<cplx> cc(n << 1);
+    for (int i = 0; i < n << 1; ++i) cc[i] = ca[i] * cb[i];
+    ifft(cc, n << 1);
+    vector<int> c(n << 1);
+    for (int i = 0; i < n << 1; ++i) c[i] = (int)(cc[i].real() + 0.5);
+    return c;
 }
