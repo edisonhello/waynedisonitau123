ae7a8768f7ed173a7b61a2db5064a6ebc48cbd47
TaWeiTu <tu.da.wei@gmail.com>
2018-07-08 15:26:10 +0800

    Add Prime counting, IncStack, Pragma

---
+++ basic/IncStack.cpp
@@ -0,0 +1,8 @@
+const int size = 256 << 20;
+register long rsp asm("rsp");
+char *p = (char*)malloc(size) + size, *bak = (char*)rsp;
+__asm__("movq %0, %%rsp\n"::"r"(p));
+
+// main
+
+__asm__("movq %0, %%rsp\n"::"r"(bak));
---
+++ basic/Pragma.cpp
@@ -0,0 +1,7 @@
+#pragma GCC optimize("Ofast", "no-stack-protector", "no-math-errno", "unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+
+#pragma warning(disable:4996)
+#pragma comment(linker, "/STACK:336777216")
+
+#pragma GCC ivdep
