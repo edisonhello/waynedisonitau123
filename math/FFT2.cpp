65f8dee16ecc7f2d6b5a7a831c2da43fcc2fc60c
TaWeiTu <tu.da.wei@gmail.com>
2018-07-18 14:54:58 +0800

    NTT

---
+++ math/FFT2.cpp
@@ -0,0 +1,70 @@
+struct cplx {
+    double re, im;
+    cplx(): re(0), im(0) {}
+    cplx(double r, double i): re(r), im(i) {}
+    cplx operator+(const cplx &rhs) const { return cplx(re + rhs.re, im + rhs.im); }
+    cplx operator-(const cplx &rhs) const { return cplx(re - rhs.re, im - rhs.im); }
+    cplx operator*(const cplx &rhs) const { return cplx(re * rhs.re - im * rhs.im, re * rhs.im + im * rhs.re); }
+    cplx conj() const { return cplx(re, -im); }
+}; 
+
+const int maxn = 262144;
+const double pi = acos(-1);
+cplx omega[maxn + 1];
+
+void prefft() {
+    for (int i = 0; i <= maxn; ++i)
+        omega[i] = cplx(cos(2 * pi * i / maxn), sin(2 * pi * i / maxn));
+}
+
+void bitrev(vector<cplx> &v, int n) {
+    int z = __builtin_ctz(n) - 1;
+    for (int i = 0; i < n; ++i) {
+        int x = 0;
+        for (int j = 0; (1 << j) < n; ++j) x ^= (((i >> j & 1)) << (z - j));
+        if (x > i) swap(v[x], v[i]);
+    }
+}
+
+void fft(vector<cplx> &v, int n) {
+    bitrev(v, n);
+    for (int s = 2; s <= n; s <<= 1) {
+        int z = s >> 1;
+        for (int i = 0; i < n; i += s) {
+            for (int k = 0; k < z; ++k) {
+                cplx x = v[i + z + k] * omega[maxn / s * k];
+                v[i + z + k] = v[i + k] - x;
+                v[i + k] = v[i + k] + x;
+            }
+        }
+    }
+}
+
+void ifft(vector<cplx> &v, int n) {
+    fft(v, n);
+    reverse(v.begin() + 1, v.end());
+    for (int i = 0; i < n; ++i) v[i] = v[i] * cplx(1. / n, 0);
+}
+
+vector<int> conv(const vector<int> &a, const vector<int> &b) {
+    int sz = 1; 
+    while (sz < a.size() + b.size() - 1) sz <<= 1;
+    vector<cplx> v(sz);
+    for (int i = 0; i < sz; ++i) {
+        double re = i < a.size() ? a[i] : 0;
+        double im = i < b.size() ? b[i] : 0;
+        v[i] = cplx(re, im);
+    }
+    fft(v, sz);
+    for (int i = 0; i <= sz / 2; ++i) {
+        int j = (sz - i) & (sz - 1);
+        cplx x = (v[i] + v[j].conj()) * (v[i] - v[j].conj()) * cplx(0, -0.25);
+        if (j != i) v[j] = (v[j] + v[i].conj()) * (v[j] - v[i].conj()) * cplx(0, -0.25);
+        v[i] = x;
+    }
+    ifft(v, sz);
+    vector<int> c(sz);
+    for (int i = 0; i < sz; ++i) c[i] = round(v[i].re);
+    while (c.size() && c.back() == 0) c.pop_back();
+    return c;
+}
--- math/NTT.cpp
+++ math/NTT.cpp
@@ -1,46 +1,63 @@
-const long long p = 2013265921, root = 31;
+const int maxn = 262144;
+const long long mod = 2013265921, root = 31;
 long long omega[maxn + 1];
 
 long long fpow(long long a, long long n) {
-    long long ret = 1ll;
+    (n += mod - 1) %= mod - 1;
+    long long r = 1;
     for (; n; n >>= 1) {
-        if (n & 1) ret = ret * a % p;
-        a = a * a % p;
+        if (n & 1) (r *= a) %= mod;
+        (a *= a) %= mod;
     }
-    return ret;
+    return r;
 }
 
 void prentt() {
+    long long x = fpow(root, (mod - 1) / maxn);
     omega[0] = 1;
-    long long r = fpow(root, (p - 1) / maxn);
-    for (int i = 1; i <= maxn; ++i) omega[i] = omega[i - 1] * r % p;
+    for (int i = 1; i <= maxn; ++i)
+        omega[i] = omega[i - 1] * x % mod;
 }
 
-void ntt(vector<long long>& a, int n, bool inv = false) {
-    int basic = maxn / n;
-    int theta = basic;
-    for (int m = n; m >= 2; m >>= 1) {
-        int mh = m >> 1;
-        for (int i = 0; i < mh; ++i) {
-            long long w = omega[i * theta % maxn];
-            for (int j = i; j < n; j += m) {
-                int k = j + mh;
-                long long x = a[j] - a[k];
-                if (x < 0) x += p;
-                a[j] += a[k];
-                if (a[j] > p) a[j] -= p;
-                a[k] = w * x % p;
+void bitrev(vector<long long> &v, int n) {
+    int z = __builtin_ctz(n) - 1;
+    for (int i = 0; i < n; ++i) {
+        int x = 0;
+        for (int j = 0; j <= z; ++j) x ^= ((i >> j & 1) << (z - j));
+        if (x > i) swap(v[x], v[i]);
+    }
+}
+
+void ntt(vector<long long> &v, int n) {
+    bitrev(v, n);
+    for (int s = 2; s <= n; s <<= 1) {
+        int z = s >> 1;
+        for (int i = 0; i < n; i += s) {
+            for (int k = 0; k < z; ++k) {
+                long long x = v[i + k + z] * omega[maxn / s * k] % mod; 
+                v[i + k + z] = (v[i + k] + mod - x) % mod;
+                (v[i + k] += x) %= mod; 
             }
         }
-        theta = theta * 2 % maxn;
     }
-    int i = 0;
-    for (int j = 1; j < n - 1; ++j) {
-        for (int k = n >> 1; k > (i ^= k); k >>= 1);
-        if (j < i) swap(a[i], a[j]);
-    }
-    if (!inv) return;
-    long long ni = fpow(n, p - 2);
-    reverse(a.begin() + 1, a.end());
-    for (int i = 0; i < n; ++i) a[i] = a[i] * ni % p;
+}
+
+void intt(vector<long long> &v, int n) {
+    ntt(v, n);
+    reverse(v.begin() + 1, v.end());
+    long long inv = fpow(n, mod - 2);
+    for (int i = 0; i < n; ++i) (v[i] *= inv) %= mod;
+}
+
+vector<long long> conv(vector<long long> a, vector<long long> b) {
+    int sz = 1;
+    while (sz < a.size() + b.size() - 1) sz <<= 1;
+    vector<long long> c(sz);
+    while (a.size() < sz) a.push_back(0);
+    while (b.size() < sz) b.push_back(0);
+    ntt(a, sz), ntt(b, sz);
+    for (int i = 0; i < sz; ++i) c[i] = a[i] * b[i] % mod;
+    intt(c, sz);
+    while (c.size() && c.back() == 0) c.pop_back();
+    return c;
 }
