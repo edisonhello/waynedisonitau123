b7945aa171a8a283c0fc159a4b6ade1877b225bb
TaWeiTu <tu.da.wei@gmail.com>
2018-09-15 16:48:26 +0800

    refactor suffix array

--- string/SuffixArray.cpp
+++ string/SuffixArray.cpp
@@ -1,52 +1,38 @@
-struct SuffixArray {
-    int sa[maxn], tmp[2][maxn], c[maxn], _lcp[maxn], r[maxn], n;
-    string s;
-    SparseTable st;
-    void suffixarray() {
-         int* rank = tmp[0];
-        int* nRank = tmp[1];
-        int A = 128;
-        for (int i = 0; i < A; ++i) c[i] = 0;
-        for (int i = 0; i < s.length(); ++i) c[rank[i] = s[i]]++;
-        for (int i = 1; i < A; ++i) c[i] += c[i - 1];
-        for (int i = s.length() - 1; i >= 0; --i) sa[--c[s[i]]] = i;
-        for (int n = 1; n < s.length(); n *= 2) {
-            for (int i = 0; i < A; ++i) c[i] = 0;
-            for (int i = 0; i < s.length(); ++i) c[rank[i]]++;
-            for (int i = 1; i < A; ++i) c[i] += c[i - 1];
-            int* sa2 = nRank;
-            int r = 0;
-            for (int i = s.length() - n; i < s.length(); ++i) sa2[r++] = i;
-            for (int i = 0; i < s.length(); ++i) if (sa[i] >= n) sa2[r++] = sa[i] - n;
-            for (int i = s.length() - 1; i >= 0; --i) sa[--c[rank[sa2[i]]]] = sa2[i];
-            nRank[sa[0]] = r = 0;
-            for (int i = 1; i < s.length(); ++i) {
-                if (!(rank[sa[i - 1]] == rank[sa[i]] && sa[i - 1] + n < s.length() && rank[sa[i - 1] + n] == rank[sa[i] + n])) r++;
-                nRank[sa[i]] = r;
-            }
-            swap(rank, nRank);
-            if (r == s.length() - 1) break;
-            A = r + 1;
+int sa[maxn], tmp[2][maxn], c[maxn], hi[maxn], r[maxn];
+// sa[i]: sa[i]-th suffix is the i-th lexigraphically smallest suffix.
+// hi[i]: longest common prefix of suffix sa[i] and suffix sa[i - 1].
+void build(const string &s) {
+    int *rnk = tmp[0], *rkn = tmp[1];
+    for (int i = 0; i < 256; ++i) c[i] = 0;
+    for (int i = 0; i < s.size(); ++i) c[rnk[i] = s[i]]++;
+    for (int i = 1; i < 256; ++i) c[i] += c[i - 1];
+    for (int i = s.size() - 1; i >= 0; --i) sa[--c[s[i]]] = i;
+    int sigma = 256;
+    for (int n = 1; n < s.size(); n *= 2) {
+        for (int i = 0; i < sigma; ++i) c[i] = 0;
+        for (int i = 0; i < s.size(); ++i) c[rnk[i]]++;
+        for (int i = 1; i < sigma; ++i) c[i] += c[i - 1];
+        int *sa2 = rkn;
+        int r = 0;
+        for (int i = s.size() - n; i < s.size(); ++i) sa2[r++] = i;
+        for (int i = 0; i < s.size(); ++i) {
+            if (sa[i] >= n) sa2[r++] = sa[i] - n;
         }
-    }
-    void solve() {
-        suffixarray();
-        for (int i = 0; i < n; ++i) r[sa[i]] = i;
-        int ind = 0; _lcp[0] = 0;
-        for (int i = 0; i < n; ++i) {
-            if (!r[i]) { ind = 0; continue; }
-            while (i + ind < n && s[i + ind] == s[sa[r[i] - 1] + ind]) ++ind;
-            _lcp[r[i]] = ind ? ind-- : 0;
+        for (int i = s.size() - 1; i >= 0; --i) sa[--c[rnk[sa2[i]]]] = sa2[i];
+        rkn[sa[0]] = r = 0;
+        for (int i = 1; i < s.size(); ++i) {
+            if (!(rnk[sa[i - 1]] == rnk[sa[i]] && sa[i - 1] + n < s.size() && rnk[sa[i - 1] + n] == rnk[sa[i] + n])) r++;
+            rkn[sa[i]] = r;
         }
-        st = SparseTable(n, _lcp);
+        swap(rnk, rkn);
+        if (r == s.size() - 1) break;
+        sigma = r + 1;
     }
-    int lcp(int L, int R) {
-        if (L == R) return n - L - 1;
-        L = r[L]; R = r[R];
-        if (L > R) swap(L, R);
-        ++L;
-        return st.query(L, R);
+    for (int i = 0; i < s.size(); ++i) r[sa[i]] = i;
+    int ind = 0; hi[0] = 0;
+    for (int i = 0; i < s.size(); ++i) {
+        if (!r[i]) { ind = 0; continue; }
+        while (i + ind < s.size() && s[i + ind] == s[sa[r[i] - 1] + ind]) ++ind;
+        hi[r[i]] = ind ? ind-- : 0;
     }
-    SuffixArray(string s): s(s), n(s.length()) {}
-    SuffixArray() {}
-};
+}
