9ff13d6ac9a7f2805bc3bb26a6223b33e58b55ea
TaWeiTu <tu.da.wei@gmail.com>
2018-09-26 22:04:34 +0800

    Add Hilber's curve

---
+++ Codes/problem/HilbertCurve.cpp
@@ -0,0 +1,16 @@
+long long hilbert(int n, int x, int y) {
+    long long res = 0;
+    for (int s = n / 2; s; s >>= 1) {
+        int rx = (x & s) > 0;
+        int ry = (y & s) > 0;
+        res += s * 1ll * s * ((3 * rx) ^ ry);
+        if (ry == 0) {
+            if (rx == 1) {
+                x = s - 1 - x;
+                y = s - 1 - y;
+            }
+            swap(x, y);
+        }
+    }
+    return res;
+}
