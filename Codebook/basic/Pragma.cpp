9622096a936b8b2bc9fb35e2d22be78d7c335f39
edisonhello <edisonhello@hotmail.com>
2018-09-28 16:39:19 +0800

    add untest pragma

--- Codebook/basic/Pragma.cpp
+++ Codebook/basic/Pragma.cpp
@@ -1,7 +1,6 @@
 #pragma GCC optimize("Ofast", "no-stack-protector", "no-math-errno", "unroll-loops")
-#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,sse4.2,popcnt,abm,mmx,avx,tune=native,arch=core-avx2,tune=core-avx2")
 
 #pragma warning(disable:4996)
-#pragma comment(linker, "/STACK:336777216")
 
 #pragma GCC ivdep
