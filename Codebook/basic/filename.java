f68615bbcbd5f3b7d650fa809d198a4d6cb00f4d
edisonhello <edisonhello@hotmail.com>
2018-11-09 10:46:28 +0800

    modify java template

--- Codebook/basic/filename.java
+++ Codebook/basic/filename.java
@@ -6,24 +6,34 @@ import java.math.*;
 public class filename{
     static Scanner in = new Scanner(System.in);
     public static void main(String[] args) {
+        int t = 7122;
         while(in.hasNext()) {
-            int a = in.nextInt();
+            t = in.nextInt();
             float b = in.nextFloat();
             String str = in.nextLine(); // getline
             String stu = in.next(); // string
         }
         System.out.println("Case #" + t);
         System.out.printf("%d\n", 7122);
-        int c = new int[5];
-        int[] e = {5,8,5,8,6,4};
+        int[] c = new int[5];
         int[][] d = {{7,1,2,2},{8,7}};
         int g = Integer.parseInt("-123");
 
         long f = (long)d[0][2];
 
-        Array.sort(e, new cmp());
-        for(int i = 0; i < e.length; ++i) {
-            System.out.print(e[i]);
+        List<Integer> l = new ArrayList<>();
+        Random rg = new Random();
+        for (int i = 9; i >= 0; --i) {
+            l.add(Integer.valueOf(rg.nextInt(100) + 1));
+            l.add(Integer.valueOf((int)(Math.random() * 100) + 1));
+        }
+        Collections.sort(l, new Comparator<Integer>() {
+            public int compare(Integer a, Integer b) {
+                return a - b;
+            }
+        });
+        for (int i = 0; i < l.size(); ++i) {
+            System.out.print(l.get(i));
         }
 
         Set<String> s = new HashSet<String>(); // TreeSet
@@ -31,11 +41,11 @@ public class filename{
         System.out.println(s);
         System.out.println(s.contains("jizz"));
 
-        Map<String, Integer> m = new Hashmap<String, Integer>();
+        Map<String, Integer> m = new HashMap<String, Integer>();
         m.put("lol", 7122);
         System.out.println(m);
-        for(String key: map.keySet()) {
-            System.out.println(key + " : " + map.get(key));
+        for(String key: m.keySet()) {
+            System.out.println(key + " : " + m.get(key));
         }
         System.out.println(m.containsKey("lol"));
         System.out.println(m.containsValue(7122));
@@ -43,23 +53,18 @@ public class filename{
         System.out.println(Math.PI);
         System.out.println(Math.acos(-1));
 
-        BigInteger bi = in.nextBigInteger(), bj = new BigInteger("-712222222222222"), bk = BigInteger.valueOf(17171);
+        BigInteger bi = in.nextBigInteger(), bj = new BigInteger("-7122"), bk = BigInteger.valueOf(17171);
         bi = bi.add(bj);
-        bi = bi.substract(BigInteger.ONE);
+        bi = bi.subtract(BigInteger.ONE);
         bi = bi.multiply(bj);
         bi = bi.divide(bj);
         bi = bi.and(bj);
         bi = bi.gcd(bj);
         bi = bi.max(bj);
-        bi = bi.pow(bj);
-        int meow = bi.compareTo(bj);
+        bi = bi.pow(10);
+        int meow = bi.compareTo(bj); // -1 0 1
         String stz = "f5abd69150";
-        BigInteger b16 = new Biginteger(stz, 16);
-        System.out.println(stz.toString(2));
-    }
-    static class cmp implements Comparator<Integer> {
-        public int compare(Integer a, Integer b) {
-            return b - a;
-        }
+        BigInteger b16 = new BigInteger(stz, 16);
+        System.out.println(b16.toString(2));
     }
 }
