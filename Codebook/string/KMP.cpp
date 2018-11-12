8612281f96311d3d98b81839da9160fc5e94e4b9
TaWeiTu <tu.da.wei@gmail.com>
2018-11-12 18:35:24 +0800

    refactor KMP, add comments

--- Codebook/string/KMP.cpp
+++ Codebook/string/KMP.cpp
@@ -1,14 +1,24 @@
-int f[maxn];
+vector<int> kmp(const string &s) {
+    vector<int> f(s.size(), 0);
+    // f[i] = length of the longest prefix (excluding s[0:i]) such that it coincides with the suffix of s[0:i] of the same length
+    // i - f[i] is the length of the smallest recurring period of s[0:i]
+    int k = 0;
+    for (int i = 1; i < (int)s.size(); ++i) {
+        while (k > 0 && s[i] != s[k]) k = f[k - 1];
+        if (s[i] == s[k]) ++k;
+        f[i] = k;
+    }
+    return f;
+}
 
-int kmp(const string& a, const string& b) {
-    f[0] = -1; f[1] = 0;
-    for (int i = 1, j = 0; i < b.size() - 1; f[++i] = ++j) {
-        if (b[i] == b[j]) f[i] = f[j];
-        while (j != -1 && b[i] != b[j]) j = f[j];
+vector<int> search(const string &s, const string &t) {
+    // return 0-indexed occurrence of t in s
+    vector<int> f = kmp(t), res;
+    int k = 0;
+    for (int i = 0; i < (int)s.size(); ++i) {
+        while (k > 0 && (k == (int)t.size() || s[i] != t[k])) k = f[k - 1];
+        if (s[i] == t[k]) ++k;
+        if (k == (int)t.size()) res.push_back(i - t.size() + 1);
     }
-    for (int i = 0, j = 0; i - j + b.size() <= a.size(); ++i, ++j) {
-        while (j != -1 && a[i] != b[j]) j = f[j];
-        if (j == b.size() - 1) return i - j;
-    }
-    return -1;
+    return res;
 }
