1d10a06303a26e1aedd4a556460ff4bb3bab139b
TaWeiTu <tu.da.wei@gmail.com>
2018-12-04 17:16:17 +0800

    Test De-Bruijn

--- Codebook/math/DeBruijn.cpp
+++ Codebook/math/DeBruijn.cpp
@@ -1,29 +1,29 @@
-int res[maxn], aux[maxn], sz; // >= k^n
+int res[maxn], aux[maxn], a[maxn], sz;
 
-int db(int t, int p) {
+void db(int t, int p, int n, int k) {
+    if (sz >= tg) return;
     if (t > n) {
         if (n % p == 0) {
-            for (int i = 1; i <= p; ++i) res[sz++] = aux[i];
+            for (int i = 1; i <= p && sz < tg; ++i) res[sz++] = aux[i];
         }
     } else {
         aux[t] = aux[t - p];
-        db(t + 1, p);
+        db(t + 1, p, n, k);
         for (int i = aux[t - p] + 1; i < k; ++i) {
             aux[t] = i;
-            db(t + 1, t);
+            db(t + 1, t, n, k);
         }
     }
 }
 
 int de_bruijn(int k, int n) {
     // return cyclic string of length k^n such that every string of length n using k character appears as a substring.
-	if(k == 1) {
-		res[0] = 0;
-		return 1;
-	}
-	for (int i = 0; i < k * n; i++) aux[i] = 0;
-	sz = 0;
-	db(1, 1);
-	return sz;
+    if (k == 1) {
+        res[0] = 0;
+        return 1;
+    }
+    for (int i = 0; i < k * n; i++) aux[i] = 0;
+    sz = 0;
+    db(1, 1, n, k);
+    return sz;
 }
-
