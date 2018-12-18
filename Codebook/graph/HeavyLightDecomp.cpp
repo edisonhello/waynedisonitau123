0d30911753a0355890ad13f3624bf3203f167010
TaWeiTu <tu.da.wei@gmail.com>
2018-12-18 16:49:25 +0800

    Shorten Heavy-Light Decomposition and test Polynomial Division

--- Codebook/graph/HeavyLightDecomp.cpp
+++ Codebook/graph/HeavyLightDecomp.cpp
@@ -1,62 +1,44 @@
-struct HeavyLightDecomp {
-    vector<int> G[maxn];
-    int tin[maxn], top[maxn], dep[maxn], maxson[maxn], sz[maxn], p[maxn], n, clk;
-    void dfs(int now, int fa, int d) {
-        dep[now] = d;
-        maxson[now] = -1;
-        sz[now] = 1;
-        p[now] = fa;
-        for (int u : G[now]) if (u != fa) {
-            dfs(u, now, d + 1);
-            sz[now] += sz[u];
-            if (maxson[now] == -1 || sz[u] > sz[maxson[now]]) maxson[now] = u;
+vector<int> g[maxn];
+int dep[maxn], sz[maxn], to[maxn], fa[maxn], fr[maxn], dfn[maxn];
+
+void dfs(int x, int p) {
+    dep[x] = ~p ? dep[p] + 1 : dep[x];
+    sz[x] = 1;
+    to[x] = -1;
+    fa[x] = p;
+    for (const int &u : g[x]) {
+        if (u == p) continue;
+        dfs(u, x);
+        sz[x] += sz[u];
+        if (to[x] == -1 || sz[to[x]] < sz[u]) to[x] = u;
+    }
+}
+
+void hld(int x, int t) {
+    static int tk = 0;
+    fr[x] = t;
+    dfn[x] = tk++;
+    if (!~to[x]) return;
+    hld(to[x], t);
+    for (const int &u : g[x]) {
+        if (u == fa[x] || u == to[x]) continue;
+        hld(u, u);
+    }
+}
+
+vector<pair<int, int>> get(int x, int y) {
+    int fx = fr[x], fy = fr[y];
+    vector<pair<int, int>> res;
+    while (fx != fy) {
+        if (dep[fx] < dep[fy]) {
+            swap(fx, fy);
+            swap(x, y);
         }
+        res.emplace_back(dfn[fx], dfn[x] + 1);
+        x = fa[fx];
+        fx = fr[x];
     }
-    void link(int now, int t) {
-        top[now] = t;
-        tin[now] = ++clk;
-        if (maxson[now] == -1) return;
-        link(maxson[now], t);
-        for (int u : G[now]) if (u != p[now]) {
-            if (u == maxson[now]) continue;
-            link(u, u);
-        }
-    }
-    HeavyLightDecomp(int n): n(n) {
-        clk = 0;
-        memset(tin, 0, sizeof(tin)); memset(top, 0, sizeof(top)); memset(dep, 0, sizeof(dep));
-        memset(maxson, 0, sizeof(maxson)); memset(sz, 0, sizeof(sz)); memset(p, 0, sizeof(p));
-    }
-    void add_edge(int a, int b) {
-        G[a].push_back(b);
-        G[b].push_back(a);
-    }
-    void solve() {
-        dfs(0, -1, 0);
-        link(0, 0);
-    }
-    int lca(int a, int b) {
-        int ta = top[a], tb = top[b];
-        while (ta != tb) {
-            if (dep[ta] < dep[tb]) {
-                swap(ta, tb); swap(a, b);
-            }
-            a = p[ta]; ta = top[a];
-        }
-        if (a == b) return a;
-        return dep[a] < dep[b] ? a : b;
-    }
-    vector<pair<int, int>> get_path(int a, int b) {
-        int ta = top[a], tb = top[b];
-        vector<pair<int, int>> ret;
-        while (ta != tb) {
-            if (dep[ta] < dep[tb]) {
-                swap(ta, tb); swap(a, b);
-            }
-            ret.push_back(make_pair(tin[ta], tin[a]));
-            a = p[ta]; ta = top[a];
-        }
-        ret.push_back(make_pair(min(tin[a], tin[b]), max(tin[a], tin[b])));
-        return ret;
-    }
-};
+    res.emplace_back(min(dfn[x], dfn[y]), max(dfn[x], dfn[y]) + 1);
+    int lca = (dep[x] < dep[y] ? x : y);
+    return res;
+}
