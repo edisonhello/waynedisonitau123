bb405c9029f81a2a20f8c849a8647a672709e8fd
edisonhello <edisonhello@hotmail.com>
2019-08-07 04:10:30 +0800

    shorten hpi

--- Codebook/geometry/_basic.cpp
+++ Codebook/geometry/_basic.cpp
@@ -1,29 +1,27 @@
-bool same(const double a,const double b){ return abs(a-b)<1e-9; }
+bool same(double a, double b) { return abs(a - b) < eps; }
 
-struct Point{
-    double x,y;
-    Point():x(0),y(0){}
-    Point(double x,double y):x(x),y(y){}
-};
-Point operator+(const Point a,const Point b){ return Point(a.x+b.x,a.y+b.y); }
-Point operator-(const Point a,const Point b){ return Point(a.x-b.x,a.y-b.y); }
-Point operator*(const Point a,const double b){ return Point(a.x*b,a.y*b); }
-Point operator/(const Point a,const double b){ return Point(a.x/b,a.y/b); }
-double operator^(const Point a,const Point b){ return a.x*b.y-a.y*b.x; }
-double abs(const Point a){ return sqrt(a.x*a.x+a.y*a.y); }
-
-
-struct Line{
-    // ax+by+c=0
-    double a,b,c;
-    double angle;
-    Point pa,pb;
-    Line():a(0),b(0),c(0),angle(0),pa(),pb(){}
-    Line(Point pa,Point pb):a(pa.y-pb.y),b(pb.x-pa.x),c(pa^pb),angle(atan2(-a,b)),pa(pa),pb(pb){}
+struct P {
+    double x, y;
+    P() : x(0), y(0) {}
+    P(double x, double y) : x(x), y(y) {}
+    P operator + (P b) { return P(x + b.x, y + b.y); }
+    P operator - (P b) { return P(x - b.x, y - b.y); }
+    P operator * (double b) { return P(x * b, y * b); }
+    P operator / (double b) { return P(x / b, y / b); }
+    double operator ^ (P b) { return x * b.y - y * b.x; }
+    double abs() { return hypot(x, y); }
 };
 
-Point intersect(Line la,Line lb){
-    if(same(la.a*lb.b,la.b*lb.a))return Point(7122,7122);
-    double bot=-la.a*lb.b+la.b*lb.a;
-    return Point(-la.b*lb.c+la.c*lb.b,la.a*lb.c-la.c*lb.a)/bot;
+struct L{
+    // ax + by + c = 0
+    double a, b ,c, o;
+    P pa, pb;
+    L() : a(0), b(0), c(0), o(0), pa(), pb() {}
+    L(P pa, P pb) : a(pa.y - pb.y), b(pb.x - pa.x), c(pa ^ pb), o(atan2(-a, b)), pa(pa), pb(pb) {}
+};
+
+P intersect(L la, L lb) {
+    if (same(la.a * lb.b, la.b * lb.a)) throw;
+    double b = -la.a * lb.b + la.b * lb.a;
+    return P(-la.b * lb.c + la.c * lb.b, la.a * lb.c - la.c * lb.a) / b;
 }
