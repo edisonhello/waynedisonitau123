72076d1dbeb07fc04293306794e31d89c4dcc67e
edisonhello <edisonhello@hotmail.com>
2019-08-09 21:29:15 +0800

    Codebook: add some function about Geometry

--- Codebook/geometry/_basic.cpp
+++ Codebook/geometry/_basic.cpp
@@ -8,8 +8,10 @@ struct P {
     P operator - (P b) { return P(x - b.x, y - b.y); }
     P operator * (double b) { return P(x * b, y * b); }
     P operator / (double b) { return P(x / b, y / b); }
+    double operator * (P b) { return x * b.x + y * b.y; }
     double operator ^ (P b) { return x * b.y - y * b.x; }
     double abs() { return hypot(x, y); }
+    P unit() { return *this / abs(); }
 };
 
 struct L{
@@ -18,10 +20,11 @@ struct L{
     P pa, pb;
     L() : a(0), b(0), c(0), o(0), pa(), pb() {}
     L(P pa, P pb) : a(pa.y - pb.y), b(pb.x - pa.x), c(pa ^ pb), o(atan2(-a, b)), pa(pa), pb(pb) {}
+    P project(P p) { return pa + (pb - pa).unit() * ((pb - pa) * (p - pa) / (pb - pa).abs()); }
+    P reflect(P p) { return p + (project(p) - p) * 2; }
 };
 
-P intersect(L la, L lb) {
-    if (same(la.a * lb.b, la.b * lb.a)) throw;
-    double b = -la.a * lb.b + la.b * lb.a;
-    return P(-la.b * lb.c + la.c * lb.b, la.a * lb.c - la.c * lb.a) / b;
+P intersect(L x, L y) {
+    if (same(x.a * y.b, x.b * y.a)) throw;
+    return P(-x.b * y.c + x.c * y.b, x.a * y.c - x.c * y.a) / (-x.a * y.b + x.b * y.a);
 }
