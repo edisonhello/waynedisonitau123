51ac481cde99ff4ef46c535d63e604858ccaabca
TaWeiTu <tu.da.wei@gmail.com>
2019-09-07 01:19:38 +0800

    fit codebook into 25 pages

--- Codebook/misc/DLX.cpp
+++ Codebook/misc/DLX.cpp
@@ -1,5 +1,4 @@
 namespace dlx {
-const int maxn = 1000 * 1000 + 5;
 int lt[maxn], rg[maxn], up[maxn], dn[maxn], cl[maxn], rw[maxn], bt[maxn], s[maxn], head, sz, ans;
 void init(int c) {
     for (int i = 0; i < c; ++i) {
@@ -8,11 +7,9 @@ void init(int c) {
         rg[i] = i == c - 1 ? c : i + 1;
         s[i] = 0;
     }
-    rg[c] = 0;
-    lt[c] = c - 1;
+    rg[c] = 0, lt[c] = c - 1;
     up[c] = dn[c] = -1;
-    head = c;
-    sz = c + 1;
+    head = c, sz = c + 1;
 }
 void insert(int r, const vector<int> &col) {
     if (col.empty()) return;
@@ -20,23 +17,19 @@ void insert(int r, const vector<int> &col) {
     for (int i = 0; i < (int)col.size(); ++i) {
         int c = col[i], v = sz++;
         dn[bt[c]] = v;
-        up[v] = bt[c];
-        bt[c] = v;
+        up[v] = bt[c], bt[c] = v;
         rg[v] = (i + 1 == (int)col.size() ? f : v + 1);
-        rw[v] = r;
-        cl[v] = c;
+        rw[v] = r, cl[v] = c;
         ++s[c];
         if (i > 0) lt[v] = v - 1;
     }
     lt[f] = sz - 1;
 }
 void remove(int c) {
-    lt[rg[c]] = lt[c];
-    rg[lt[c]] = rg[c];
+    lt[rg[c]] = lt[c], rg[lt[c]] = rg[c];
     for (int i = dn[c]; i != c; i = dn[i]) {
         for (int j = rg[i]; j != i; j = rg[j]) {
-            up[dn[j]] = up[j];
-            dn[up[j]] = dn[j];
+            up[dn[j]] = up[j], dn[up[j]] = dn[j];
             --s[cl[j]];
         }
     }
@@ -45,19 +38,15 @@ void restore(int c) {
     for (int i = up[c]; i != c; i = up[i]) {
         for (int j = lt[i]; j != i; j = lt[j]) {
             ++s[cl[j]];
-            up[dn[j]] = j;
-            dn[up[j]] = j;
+            up[dn[j]] = j, dn[up[j]] = j;
         }
     }
-    lt[rg[c]] = c;
-    rg[lt[c]] = c;
+    lt[rg[c]] = c, rg[lt[c]] = c;
 }
 // Call dlx::make after inserting all rows.
 void make(int c) {
-    for (int i = 0; i < c; ++i) {
-        dn[bt[i]] = i;
-        up[i] = bt[i];
-    }
+    for (int i = 0; i < c; ++i)
+        dn[bt[i]] = i, up[i] = bt[i];
 }
 void dfs(int dep) {
     if (dep >= ans) return;
@@ -77,8 +66,6 @@ void dfs(int dep) {
     restore(w);
 }
 int solve() {
-  ans = 1e9;
-  dfs(0);
+  ans = 1e9, dfs(0);
   return ans;
 }}
-
