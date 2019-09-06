7a30bc02f53c4affce8cb48fd6d603d0ee30ce66
edisonhello <edisonhello@hotmail.com>
2019-09-06 10:01:36 +0800

    Update pointInPolygon

--- Codebook/geometry/pointInPolygon.cpp
+++ Codebook/geometry/pointInPolygon.cpp
@@ -1,11 +1,13 @@
-bool pip(vector<P> ps, P p) {
+int pip(vector<P> ps, P p) {
     int c = 0;
     for (int i = 0; i < ps.size(); ++i) {
-        int a = i;
-        int b = (i + 1) % ps.size();
+        int a = i, b = (i + 1) % ps.size();
+        L l(ps[a], ps[b]);
+        P q = l.project(p);
+        if ((p - q).abs() < eps && l.inside(q)) return 1;
         if (same(ps[a].y, ps[b].y) && same(ps[a].y, p.y)) continue;
-        if (ps[a].y > ps[b].y) swap(x, y);
+        if (ps[a].y > ps[b].y) swap(a, b);
         if (ps[a].y <= p.y && p.y < ps[b].y && p.x <= ps[a].x + (ps[b].x - ps[a].x) / (ps[b].y - ps[a].y) * (p.y - ps[a].y)) ++c;
     }
-    return c & 1;
+    return (c & 1) * 2;
 }
