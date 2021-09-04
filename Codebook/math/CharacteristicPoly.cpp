28def82f25ca6db1a58923462a37f5190a8aaa26
TaWeiTu <tu.da.wei@gmail.com>
2021-09-04 22:31:13 +0800

    Update

---
+++ Codebook/math/CharacteristicPoly.cpp
@@ -0,0 +1,46 @@
+vector<vector<int>> Hessenberg(const vector<vector<int>> &A) {
+    int N = A.size();
+    vector<vector<int>> H = A;
+    for (int i = 0; i < N - 2; ++i) {
+        if (!H[i + 1][i]) {
+            for (int j = i + 2; j < N; ++j) {
+                if (H[j][i]) {
+                    for (int k = i; k < N; ++k) swap(H[i + 1][k], H[j][k]);
+                    for (int k = 0; k < N; ++k) swap(H[k][i + 1], H[k][j]);
+                    break;
+                }
+            }
+        }
+        if (!H[i + 1][i]) continue;
+        int val = fpow(H[i + 1][i], kP - 2);
+        for (int j = i + 2; j < N; ++j) {
+            int coef = 1LL * val * H[j][i] % kP;
+            for (int k = i; k < N; ++k) H[j][k] = (H[j][k] + 1LL * H[i + 1][k] * (kP - coef)) % kP;
+            for (int k = 0; k < N; ++k) H[k][i + 1] = (H[k][i + 1] + 1LL * H[k][j] * coef) % kP;
+        }
+    }
+    return H;
+}
+vector<int> CharacteristicPoly(const vector<vector<int>> &A) {
+    int N = A.size();
+    auto H = Hessenberg(A);
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) H[i][j] = kP - H[i][j];
+    }
+    vector<vector<int>> P(N + 1, vector<int>(N + 1));
+    P[0][0] = 1;
+    for (int i = 1; i <= N; ++i) {
+        P[i][0] = 0;
+        for (int j = 1; j <= i; ++j) P[i][j] = P[i - 1][j - 1];
+        int val = 1;
+        for (int j = i - 1; j >= 0; --j) {
+            int coef = 1LL * val * H[j][i - 1] % kP;
+            for (int k = 0; k <= j; ++k) P[i][k] = (P[i][k] + 1LL * P[j][k] * coef) % kP;
+            if (j) val = 1LL * val * (kP - H[j][j - 1]) % kP;
+        }
+    }
+    if (N & 1) {
+        for (int i = 0; i <= N; ++i) P[N][i] = kP - P[N][i];
+    }
+    return P[N];
+}
